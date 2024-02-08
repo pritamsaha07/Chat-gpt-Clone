93cc9aa12ea3a55f150f997420cd7125520de872
Pritam Saha <82364650+pritamsaha07@users.noreply.github.com>
2024-02-09 02:52:50 +0530

    Update openaiController.js

--- server/openaiController.js
+++ server/openaiController.js
@@ -1,10 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const OpenAI=require('openai')
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
 
 exports.summaryController = async (req, res) => {
   try {
